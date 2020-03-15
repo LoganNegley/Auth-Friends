399daa816f56c2eb08adea267a440a74947a9053
Logan Negley <logannegley@gmail.com>
2020-03-15 15:08:42 -0500

    LoginForm component boiler plate created

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './App.css';
+import LoginForm from '../src/components/LoginForm';
+
+
 
 function App() {
   return (
---
+++ friends/src/components/LoginForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LoginForm() {
+  return (
+    <div className='login-form'>
+      
+    </div>
+  );
+}
+
+export default LoginForm;
