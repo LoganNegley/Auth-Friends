364706631b9ad60fca5e34425d7c8d8e5a49ba7a
Logan Negley <logannegley@gmail.com>
2020-03-15 18:56:31 -0500

    axios get request to get friends from api

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
 
 function FriendsList() {
+    const [friends, setFriends] = useState([])
+
+      useEffect(() => {
+        axios.get('http://localhost:5000/api/friends')
+        .then(res => console.log(res))
+        .catch(err => console.log(err, 'error getting list of friends from api'))
+  });
+
+
   return (
     <div className="FriendsList">
       <h1>List of Friends</h1>
