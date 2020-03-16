04fdd47f37b20c703e10c024f9641a672ce133f8
Logan Negley <logannegley@gmail.com>
2020-03-15 19:41:43 -0500

    addFriend form created

--- friends/src/components/AddFriend.js
+++ friends/src/components/AddFriend.js
@@ -3,12 +3,39 @@ import {axiosWithAuth} from '../axiosAuth.js/axiosWithAuth';
 
 
 function AddFriend() {
+    const [newFriend, setNewFriend] = useState({
+    id: date.new(),
+    name: '',
+    age: '',
+    email: '',
+    });
 
 
   return (
     <div className="add-friend-form">
         <form>
-            
+            <input
+                type='text'
+                name='name'
+                placeholder='Name'
+                value={newFriend.name}
+                // onChange={}
+            />
+            <input
+                type='text'
+                name='age'
+                placeholder='Age'
+                value={newFriend.age}
+                // onChange={}
+            />
+            <input
+                type='text'
+                name='email'
+                placeholder='Email'
+                value={newFriend.email}
+                // onChange={}
+            />
+            <button type='submit'>Add A New Friend</button>
         </form>
     </div>
   );
